f20b012c7fb4806cafdbb875899d24360e28da5f
nicolasorobia <nicolasorobia@gmail.com>
2026-06-15 15:54:11 -0700

    add  ball and paddle class

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -15,20 +15,34 @@ void Update() {
     x += speed_x;
     y += speed_y;
 
-    // check collision logic for ball
+    // check collision logic for ball:
+    // bounce off top and bottom screen window
     if(y + radius >= GetScreenHeight() || y + radius <= 0) speed_y *= -1;
+    // bounce off left and right screen window
     if(x + radius >= GetScreenWidth() || x + radius <= 0) speed_x *= -1;
 
 }
+};
 
+class Paddle {
+public:
+float width = 20, height = 80;
+float x, y;
+int speed;
+
+void Draw() {
+    DrawRectangle(x, y, width, height, WHITE);
+}
 };
 
 Ball ball;
+Paddle left_paddle;
+Paddle right_paddle;
 
 int main() {
 
     const int screen_width = 1024;
-    const int screen_height = 728;
+    const int screen_height = 768;
     InitWindow(screen_width, screen_height, "Pong");
     SetTargetFPS(60);
 
@@ -38,29 +52,26 @@ int main() {
     ball.speed_x = 4;
     ball.speed_y = 4;
 
-    int rec_width = 20;
-    int rec_height = 80;
-    int padding = 10;
-    int left_paddle_xpos = padding;
-    int right_paddle_xpos = screen_width - (rec_width + padding);
-    const int y_rect_center = screen_height/2 - rec_height/2;
+    left_paddle.x = 10;
+    left_paddle.y = screen_height/2 - left_paddle.height/2;
+    left_paddle.speed = 7;
+
+    right_paddle.x = screen_width - right_paddle.width - 10;
+
+    
 
     while(WindowShouldClose() == false) {
         BeginDrawing();
 
         // Updating
         ball.Update();
-
-        // check collision logic for ball
-        // if (ball.y >= screen_height || ball.y <= 0) ball.speed_y *= -1;
-        // if (ball.x >= screen_width || ball.x <= 0) ball.speed_x *= -1;
+        
 
         // Drawing
         ClearBackground(BLACK);
-        DrawLine(screen_width/2, 0, screen_width/2, 728, WHITE);
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
         ball.Draw();
-        DrawRectangle(left_paddle_xpos, y_rect_center, rec_width, rec_height, WHITE);
-        DrawRectangle(right_paddle_xpos, y_rect_center, rec_width, rec_height, WHITE);
+        left_paddle.Draw();
 
         
 
